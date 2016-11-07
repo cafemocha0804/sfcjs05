90a29567aeee93deb273daae915e7467d54cbc58
cafemocha0804 <moka_tabata@tabatamoenatsu-no-MacBook-Air.local>
2016-11-07 21:15:21 +0900

    できた！

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,40 @@
 function isEvenNumber(n){
-    if(n%2 == 0){
-        return ture;
+    if(n % 2 == 0){
+        return true;
     }else{
         return false;
     }
 }
 
 
-function toEvenNumber(number){
-    if()
-        return
+function toEvenNumber(n){
+    if(n % 2 == 0){
+        return n;
+    }else{
+        return n + 1;
+    }
 }
 
 
-function toOddNumber(num){
-    if()
-        return
+function toOddNumber(n){
+    if(n % 2 ==! 0){
+        return n;
+    }else{
+        return n + 1;
+    }
+}
+
+
+function sumN(n) {
+  var a = [];
+  if (n>0) {
+    for (var i = 1; i <= n; i++) {
+      a.push(i);
+    }
+  }else {
+    for (var i = n; i <= 1; i++) {
+      a.push(i);
+    }
+  }
+  return a;
 }
