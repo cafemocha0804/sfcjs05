8c771898c56f95d0679bdb99a1e36ba76936ed36
cafemocha0804 <moka_tabata@tabatamoenatsu-no-MacBook-Air.local>
2016-11-07 18:08:24 +0900

    作り直し

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+function isEvenNumber(n){
+    if(n%2 == 0){
+        return ture;
+    }else{
+        return false;
+    }
+}
+
+
+function toEvenNumber(number){
+    if()
+        return
+}
+
+
+function toOddNumber(num){
+    if()
+        return
+}
