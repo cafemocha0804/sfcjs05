fed81d40baf0df4ca96c1f0a6affdb2461be74fe
cafemocha0804 <moka_tabata@tabatamoenatsu-no-MacBook-Air.local>
2016-11-08 19:18:13 +0900

    復習

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+/* 練習問題２
+数値を 1 つ引数としてとる関数
+偶数なら true 、そうでない場合は false を返す*/
 function isEvenNumber(n){
     if(n % 2 == 0){
         return true;
@@ -6,7 +9,7 @@ function isEvenNumber(n){
     }
 }
 
-
+//練習問題３　与えられた整数を偶数にする関数
 function toEvenNumber(n){
     if(n % 2 == 0){
         return n;
@@ -15,7 +18,7 @@ function toEvenNumber(n){
     }
 }
 
-
+//練習問題４　与えられた整数を奇数にする関数
 function toOddNumber(n){
     if(n % 2 ==! 0){
         return n;
@@ -24,7 +27,9 @@ function toOddNumber(n){
     }
 }
 
-
+/*練習問題７
+1から引数で指定された数以下の整数を全て返す関数
+引数には負の数が指定されることもある*/
 function sumN(n) {
   var a = [];
   if (n>0) {
